2310295f0b2088523dc84c937943793de0deeb3a
Amos Maimon <amosmim@gmail.com>
2017-04-20 14:39:37 +0300

    fix repostory

--- ap2ex1_server/Controller.cs
+++ ap2ex1_server/Controller.cs
@@ -4,60 +4,60 @@ using System.Text;
 using System.Collections.Generic;
 namespace ap2ex1_server
 {
-	public class Controller : IController
-	{
-		private Dictionary<string, ICommandable> actions;
-		private IModel model;
-		public Controller()
-		{
-			this.model = new Model();
-			this.actions = new Dictionary<string, ICommandable>();
+    public class Controller : IController
+    {
+        private Dictionary<string, ICommandable> actions;
+        private IModel model;
+        public Controller()
+        {
+            this.model = new Model();
+            this.actions = new Dictionary<string, ICommandable>();
 
-			AddNewAction("generate", new GenerateCommand(model));
-			AddNewAction("list", new ListCommand(model));
-			AddNewAction("start", new StartCommand(model));
-			AddNewAction("close", new CloseCommand(model));
-			AddNewAction("join", new JoinCommand(model));
-			AddNewAction("play", new PlayCommand(model));
-			AddNewAction("solve", new SolveCommand(model));
-		}
+            AddNewAction("generate", new GenerateCommand(model));
+            AddNewAction("list", new ListCommand(model));
+            AddNewAction("start", new StartCommand(model));
+            AddNewAction("close", new CloseCommand(model));
+            AddNewAction("join", new JoinCommand(model));
+            AddNewAction("play", new PlayCommand(model));
+            AddNewAction("solve", new SolveCommand(model));
+        }
 
-		public void AddNewAction(string actionName, ICommandable action)
-		{
-			actions.Add(actionName, action);
-		}
+        public void AddNewAction(string actionName, ICommandable action)
+        {
+            actions.Add(actionName, action);
+        }
 
-		private string[] ParseCommand(string data, char delim)
-		{
-			string[] command;
+        private string[] ParseCommand(string data, char delim)
+        {
+            string[] command;
 
-			command = data.Split(delim);
+            command = data.Split(delim);
 
-			return command;
-		}
+            return command;
+        }
 
-		public string ExecuteCommand(string command, Socket client)
-		{
-			string strData;
-			ICommandable value;
-			// Get parsed command
-			string[] actionArray = this.ParseCommand(command, ' ');
+        public string ExecuteCommand(string command, Socket client)
+        {
+            string strData;
+            ICommandable value;
+            // Get parsed command
+            string[] actionArray = this.ParseCommand(command, ' ');
 
-			// Check if command exist, if so, run it.
-			if (!actions.TryGetValue(actionArray[0], out value))
-			{
-				Console.WriteLine("Server Error: 404 option not found");
-				byte[] data = new byte[1024];
-				data = Encoding.ASCII.GetBytes("Error: 404 option not found");
-				client.Send(data, data.Length, SocketFlags.None);
-				strData = "-1";
-			}
-			else
-			{
-				strData = actions[actionArray[0]].Execute(actionArray, client);
-			}
+            // Check if command exist, if so, run it.
+            if (!actions.TryGetValue(actionArray[0], out value))
+            {
+                Console.WriteLine("Server Error: 404 option not found");
+                byte[] data = new byte[1024];
+                data = Encoding.ASCII.GetBytes("Error: 404 option not found");
+                client.Send(data, data.Length, SocketFlags.None);
+                strData = "-1";
+            }
+            else
+            {
+                strData = actions[actionArray[0]].Execute(actionArray, client);
+            }
 
-			return strData;
-		}
-	}
+            return strData;
+        }
+    }
 }
